49f17b776d94976aff914ca48676cae8ca1a29e5
Eric Krampah <ekrampah253@gmail.com>
2025-02-16 21:56:33 +0000

    react app

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
+import "./App.css";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+     <WelcomeMessage />
+      <Header /> 
+      <MainContent />
+      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </>
+  );
+}
+
+export default App;
